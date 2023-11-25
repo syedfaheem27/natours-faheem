f1e4fc0ea6ce85c257112d4dd7508e05528b185a
syedfaheem27 <syedfaheem2719@gmail.com>
2023-11-25 11:56:33 +0530

    Define schema for tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,3 @@
-const fs = require('fs');
 const Tour = require('../models/tourModel');
 
 exports.getTours = async (req, res) => {
--- models/tourModel.js
+++ models/tourModel.js
@@ -15,15 +15,53 @@ const tourSchema = mongoose.Schema({
     type: String,
     required: [true, 'A Tour must have a name'],
     unique: true,
+    trim: true,
+  },
+  duration: {
+    type: Number,
+    required: [true, 'A Tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A Tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A Tour must have difficulty'],
+  },
+
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A Tour must have a price'],
   },
-  rating: {
-    type: Number,
-    default: 4.5,
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    required: [true, 'A Tour must have a summary'],
+    trim: true,
   },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A Tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 //Creating a model for the defined schema
