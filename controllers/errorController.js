2be46d680d946481471ed4c63c8789503af4bd17
syedfaheem27 <syedfaheem2719@gmail.com>
2023-12-16 01:37:34 +0530

    Handle dev and prod errors, mongodb driver errors - validation,duplicatekey etc. and add safety nets for unhandled promise rejections and uncaught exceptions

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,64 @@
-module.exports = (err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
+const AppError = require('../utils/appError');
 
+function handleCastErrorDB(err) {
+  const message = `Invalid ${err.path.replace('_', '')}: ${err.value} `;
+  return new AppError(message, 400);
+}
+
+function handleDuplicateFieldsDB(err) {
+  const message = `Duplicate Field value: ${err.keyValue.name}. Please use another value`;
+  return new AppError(message, 400);
+}
+
+function handleValidationDB(err) {
+  const message = err.message.replace(
+    'Tour validation failed:',
+    'Invalid input data.',
+  );
+  return new AppError(message, 400);
+}
+
+function sendErrorDev(err, res) {
   res.status(err.statusCode).json({
     status: err.status,
+    error: err,
     message: err.message,
+    stack: err.stack,
   });
+}
+
+function sendErrorProd(err, res) {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.log('ERROR 🔥', err);
+
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!',
+    });
+  }
+}
+
+module.exports = (err, req, res, next) => {
+  let error = Object.create(
+    Object.getPrototypeOf(err),
+    Object.getOwnPropertyDescriptors(err),
+  );
+
+  error.statusCode = error.statusCode || 500;
+  error.status = error.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') sendErrorDev(error, res);
+
+  if (error.name === 'CastError') error = handleCastErrorDB(error);
+
+  if (error.codeName === 'DuplicateKey') error = handleDuplicateFieldsDB(error);
+
+  if (error.name === 'ValidationError') error = handleValidationDB(error);
+
+  if (process.env.NODE_ENV === 'production') sendErrorProd(error, res);
 };
--- server.js
+++ server.js
@@ -4,6 +4,18 @@ const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
+//HANDLING UNCAUGHT EXCEPTIONS
+//ERRORS THAT OCCUR IN OUR SYNCHRONOUS CODE
+
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION 💥');
+
+  console.log(err.name, err.message);
+  //our node application is in an unclean state, so need to
+  //shutdown immediately
+  process.exit(1);
+});
+
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
@@ -19,6 +31,19 @@ const port = process.env.PORT || 8000;
 
 //START SERVER
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
+});
+
+//HANDLING UNHANDLED REJECTIONS IN A GLOBAL PLACE
+//INSTEAD OF USING A CATCH BLOCK EVERYWHERE
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION 💥');
+
+  console.log(err.name, err.message);
+  server.close(() => {
+    console.log('Shutting down...');
+    process.exit(1);
+  });
 });
