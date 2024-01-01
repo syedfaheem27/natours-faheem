6d4957a5ad61103c748af37960dd2434a5741dba
syedfaheem27 <syedfaheem2719@gmail.com>
2024-01-01 20:39:48 +0530

    Add functionality to get all reviews on a particular tour

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,8 +1,12 @@
 const Review = require('../models/reviewModel');
 const catchAsync = require('../utils/catchAsync');
 
-exports.getReviews = catchAsync(async (req, res, next) => {
-  const reviews = await Review.find();
+exports.getReviews = catchAsync(async (req, res) => {
+  let filter = {};
+  console.log(req.params);
+  if (req.params.tourId) filter = { tour: req.params.tourId };
+
+  const reviews = await Review.find(filter);
 
   res.status(200).json({
     status: 'success',
@@ -13,7 +17,7 @@ exports.getReviews = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.createReview = catchAsync(async (req, res, next) => {
+exports.createReview = catchAsync(async (req, res) => {
   if (!req.body.tour) req.body.tour = req.params.tourId;
   if (!req.body.user) req.body.user = req.user._id;
 
