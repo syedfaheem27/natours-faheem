49c1568e81aac6fc449d03a81235670290458a54
syedfaheem27 <syedfaheem2719@gmail.com>
2024-01-08 13:05:36 +0530

    Add endpoint - get tours within a certain distance

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,8 +1,8 @@
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handleFactory');
+const AppError = require('../utils/appError');
 // const ApiFeatures = require('../utils/apiFeatures');
-// const AppError = require('../utils/appError');
 
 //Middleware to handle requests for the alias route
 exports.aliasTopCheap = async (req, res, next) => {
@@ -17,6 +17,35 @@ exports.createTour = factory.createOne(Tour);
 exports.getTour = factory.getOne(Tour, { path: 'reviews' });
 exports.updateTour = factory.updateOne(Tour);
 exports.deleteTour = factory.deleteOne(Tour);
+
+// /tours-within/:distance/center/:latlng/unit/:unit
+//handling geo-spatial queries
+exports.getGeoData = catchAsync(async (req, res, next) => {
+  const { distance, latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(',');
+
+  if (!lat || !lng)
+    next(
+      new AppError(
+        'Please provide latitude and longitude in the format lat,lng',
+        400,
+      ),
+    );
+
+  const radius = unit === 'mi' ? distance / 3963.2 : distance / 6378.1;
+
+  const tours = await Tour.find({
+    startLocation: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
+  });
+
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      data: tours,
+    },
+  });
+});
 
 //Using the aggregation pipeline to get the stats on tours
 exports.getTourStats = catchAsync(async (req, res, next) => {
