8cdeda78044d5d1603cdb501eb60a16e1cd54113
syedfaheem27 <syedfaheem2719@gmail.com>
2024-01-15 10:58:37 +0530

    add multer

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -49556,7 +49556,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40303" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41273" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
