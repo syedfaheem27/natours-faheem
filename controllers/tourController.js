17914160cf71276d083b2ab7fa1c962ba4be3228
syedfaheem27 <syedfaheem2719@gmail.com>
2024-01-17 12:57:20 +0530

    Add functionality to upload and process multiple images: imageCover and images for a tour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,8 +1,68 @@
+const multer = require('multer');
+const sharp = require('sharp');
+
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handleFactory');
 const AppError = require('../utils/appError');
 // const ApiFeatures = require('../utils/apiFeatures');
+
+//Handling Image uploads
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else
+    cb(new AppError('Not an image. Please upload images only.', 400), false);
+};
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+exports.uploadTourImages = upload.fields([
+  {
+    name: 'imageCover',
+    maxCount: 1,
+  },
+  {
+    name: 'images',
+    maxCount: 3,
+  },
+]);
+
+exports.resizeTourImages = catchAsync(async (req, res, next) => {
+  //If no image cover or images, just return to the next middleware
+  if (!req.files.images || !req.files.imageCover) return next();
+
+  //1. Image Cover
+  req.body.imageCover = `tour-${req.params.id}-${Date.now()}.jpeg`;
+
+  await sharp(req.files.imageCover[0].buffer)
+    .resize(1800, 1200)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/tours/${req.body.imageCover}`);
+
+  //2. Images
+  req.body.images = [];
+  await Promise.all(
+    req.files.images.map(async (file, i) => {
+      const fileName = `tour-${req.params.id}-${Date.now()}-${i + 1}.jpeg`;
+
+      await sharp(file.buffer)
+        .resize(1800, 1200)
+        .toFormat('jpeg')
+        .jpeg({ quality: 90 })
+        .toFile(`public/img/tours/${fileName}`);
+
+      req.body.images.push(fileName);
+    }),
+  );
+
+  next();
+});
 
 //Middleware to handle requests for the alias route
 exports.aliasTopCheap = async (req, res, next) => {
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,4 @@
 const multer = require('multer');
-
 const sharp = require('sharp');
 
 const User = require('../models/userModel');
@@ -22,19 +21,19 @@ const upload = multer({
 
 exports.uploadUserPhoto = upload.single('photo');
 
-exports.resizeUserPhoto = (req, res, next) => {
+exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
   if (!req.file) return next();
 
   req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
 
-  sharp(req.file.buffer)
+  await sharp(req.file.buffer)
     .resize(500, 500)
     .toFormat('jpeg')
     .jpeg({ quality: 90 })
     .toFile(`public/img/users/${req.file.filename}`);
 
   next();
-};
+});
 
 function filterUserData(data, ...dataInclude) {
   const userData = {};
