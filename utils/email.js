9ed9d55149653a616bd6795a762295d08d8f2e1a
syedfaheem27 <syedfaheem2719@gmail.com>
2024-01-18 12:48:46 +0530

    Add a robust email handler

--- utils/email.js
+++ utils/email.js
@@ -1,23 +1,55 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
-const sendEmail = async options => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.from = `Syed Faheem <${process.env.Email}>`;
+    this.url = url;
+    this.firstName = user.name.slipt(' ')[0];
+  }
 
-  const data = {
-    from: 'Syed Faheem <faheem@test.io>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-  };
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      //Use Sendgrid
+      return 1;
+    }
 
-  await transporter.sendMail(data);
-};
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
 
-module.exports = sendEmail;
+  async send(template, subject) {
+    //Generate HTML based on a template
+    const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+      from: this.from,
+      url: this.url,
+      subject,
+    });
+
+    //Define the mail options
+    const emailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.fromString(html),
+    };
+
+    //Send email
+    await this.newTransport().sendMail(emailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to the Natours family!');
+  }
+}
+
+module.exports = Email;
