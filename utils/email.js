d14e7aac492ab72d5534c0f4206f3dec073453a3
syedfaheem27 <syedfaheem2719@gmail.com>
2024-01-18 20:04:30 +0530

    Add SendInBlue to send emails in production

--- utils/email.js
+++ utils/email.js
@@ -5,15 +5,26 @@ const { htmlToText } = require('html-to-text');
 class Email {
   constructor(user, url) {
     this.to = user.email;
-    this.from = `Syed Faheem <${process.env.Email_FROM}>`;
+    // this.from = `Syed Faheem <${process.env.Email_FROM}>`;
+    this.from = process.env.EMAIL_FROM;
     this.url = url;
     this.firstName = user.name.split(' ')[0];
   }
 
   newTransport() {
     if (process.env.NODE_ENV === 'production') {
-      //Use Sendgrid
-      return 1;
+      //Use SendinBlue
+      return nodemailer.createTransport({
+        service: 'SendinBlue',
+
+        // host: process.env.SENDINBLUE_HOST,
+        // port: process.env.SENDINBLUE_PORT,
+
+        auth: {
+          user: process.env.SENDINBLUE_USERNAME,
+          pass: process.env.SENDINBLUE_PASSWORD,
+        },
+      });
     }
 
     return nodemailer.createTransport({
@@ -36,7 +47,8 @@ class Email {
 
     //Define the mail options
     const emailOptions = {
-      from: this.from,
+      // from: this.from,
+      from: 'faheem@test.io',
       to: this.to,
       subject,
       html,
