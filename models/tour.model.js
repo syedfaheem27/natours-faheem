d366e71c96b28a40ff2b6ff5f858c1b668888351
syedfaheem27 <syedfaheem2719@gmail.com>
2024-12-05 01:43:30 +0530

    Add index on tours for slugs

--- models/tour.model.js
+++ models/tour.model.js
@@ -124,6 +124,7 @@ const tourSchema = new mongoose.Schema(
 
 // tourSchema.index({ price: 1 });
 tourSchema.index({ price: 1, ratingsAverage: -1 });
+tourSchema.index({ slug: 1 });
 
 tourSchema.virtual("durationWeeks").get(function () {
   return this.duration / 7;
