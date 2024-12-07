a5d6b197353633272d843b589037b17ae6914804
syedfaheem27 <syedfaheem2719@gmail.com>
2024-12-07 12:47:17 +0530

    Make overview and tour extend base template

--- app.js
+++ app.js
@@ -62,7 +62,18 @@ if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 app.get("/", (req, res) => {
   res.status(200).render("base", {
     tour: "The Dummy Tour",
-    user: "Faheem",
+  });
+});
+
+app.get("/overview", (req, res) => {
+  res.status(200).render("overview", {
+    title: "All Tours",
+  });
+});
+
+app.get("/tour", (req, res) => {
+  res.status(200).render("tour", {
+    title: "The Forest Hiker",
   });
 });
 
